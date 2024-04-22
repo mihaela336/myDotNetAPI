3706697241d400bdbcd1a18157ba1bb26161ed6a
mihaela336 <mihaela.pasca336@gmail.com>
2024-04-22 21:10:34 +0300

    Add validations

--- api/Dtos/Station/StationDto.cs
+++ api/Dtos/Station/StationDto.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Dtos.Station
 {
@@ -9,8 +10,15 @@ namespace api.Dtos.Station
     {
         //copy paste from station model
         public int Id {get; set;}
-
+//validarions
+        [Required]
+        [MinLength(5, ErrorMessage = "Name must be 5 characters")]
+        [MaxLength(50, ErrorMessage = "Name must be under 50 characters")]
         public string Name {get; set;} = string.Empty;
+
+        [Required]
+        [MinLength(10, ErrorMessage = "Adress must be at least 10 characters")]
+        [MaxLength(50, ErrorMessage = "adress must be under 150 characters")]
         public string Adress {get; set;} = string.Empty;
 
     }
--- api/Dtos/Transaction/CreateTransactionRequestDto.cs
+++ api/Dtos/Transaction/CreateTransactionRequestDto.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace api.Dtos.Transaction
@@ -8,13 +9,21 @@ namespace api.Dtos.Transaction
     public class CreateTransactionRequestDto
     {
         // the dto will have evrithyng from the model except the id (that is auto generated)
+        //[Required]
         public int? StationId{get; set;}//TODO: check if it actually works TODO: reolace stationId with sessionId
         //navigation property -alows us to navigate whitin models
         public DateTime CreatedOn {get; set;} = DateTime.Now;
-
+        [Required]
+        [Range(0, 1000)]
         public decimal KwPrice {get; set;}
+         [Required]
+        [Range(0, 100000)]
         public decimal KwTotal {get; set;}
+                [Required]
+        [Range(0, 1000)]
         public decimal SurchargeHour {get; set;}
+                [Required]
+        [Range(0, 100000)]
         public decimal SurchargeTotal {get; set;}
     }
 }
