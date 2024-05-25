032651e112692ff29bd6a4c73d3263d1eba17dae
mihaela336 <mihaela.pasca336@gmail.com>
2024-05-25 23:00:45 +0300

    rfactor data flow add conditional rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,35 @@
+import { SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CreateStationRequestDto } from './station';
+import { searchStations } from './api';
+
 
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CreateStationRequestDto[]>([]);
+  const [serverError, setServerError] = useState<string>("");
+  const handleChange =(e: React.ChangeEvent<HTMLInputElement>)=>{
+      setSearch(e.target.value);
+      console.log(e);
+  }
+  const onClick = async (e: SyntheticEvent) =>{
+    const result = await  searchStations(search);
+    if (typeof result === "string"){
+      setServerError(result);
+
+    } else if(Array.isArray(result.data)){
+      setSearchResult(result.data);
+    }
+    console.log(setSearchResult);
+
+  };
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      {serverError &&<h1>{serverError}</h1>}
      <CardList />
     </div>
   );
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
+import React, { FormEvent, SyntheticEvent, useState } from 'react'
 
-type Props = {}
+interface Props {
+    onClick: (e: SyntheticEvent)=> void;
+    search: string | undefined;
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>)=> void;
+}
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
+const Search: React.FC<Props> = ({onClick, search, handleChange}: Props): JSX.Element => {
 
-    const [search, setSearch] = useState<string>("");
-    const handleChange =(e: React.ChangeEvent<HTMLInputElement>)=>{
-        setSearch(e.target.value);
-        console.log(e);
-    }
-    const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
-        console.log(e);
 
-    }
     return (
         <div>
             <input value={search} onChange = {(e)=> handleChange(e)}></input>
