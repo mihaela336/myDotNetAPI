0e1ac607288ddcc1e3de331a47334870b9a2da44
mihaela336 <mihaela.pasca336@gmail.com>
2024-05-27 10:28:50 +0300

    retrieve individual station from api

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,20 +1,30 @@
 import axios from "axios"
-import { CreateStationRequestDto } from "./station"
+import { CreateStationRequestDto, StationDetails } from "./station"
 interface SearchResponse {
-    data: CreateStationRequestDto [];
+    data: CreateStationRequestDto[];
+
 }
 
-export const searchStations = async(query: string) => {
+export const searchStations = async (query: string) => {
     try {
         const data = await axios.get<SearchResponse>('http://localhost:5220/api/station/');
         return data;
-    } catch (error){
-        if(axios.isAxiosError(error)){
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
             console.log(" error message: ", error.message);
             return error.message;
         } else {
             console.log("unexpected error: ", error);
             return "An unexpected error has occured";
         }
+    }
+}
+
+export const getStationDetails = async (query: string) => {
+    try {
+        const data = await axios.get<StationDetails>(`http://localhost:5220/api/station/${query}`);
+        return data;
+    } catch (error: any) {
+        console.log(" error message from API: ", error.message);
     }
 }
