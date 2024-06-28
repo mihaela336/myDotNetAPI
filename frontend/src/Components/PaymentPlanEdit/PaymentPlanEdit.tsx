614d4171fc2480578397788db441981fae5421fd
mihaela336 <mihaela.pasca336@gmail.com>
2024-06-28 10:29:46 +0300

    logout fix and commit misteriously uncommited changes

--- frontend/src/Components/PaymentPlanEdit/PaymentPlanEdit.tsx
+++ frontend/src/Components/PaymentPlanEdit/PaymentPlanEdit.tsx
@@ -79,7 +79,7 @@ const PaymentPlanEdit = (props: Props) => {
             type="submit"
             className="w-full text-white bg-lightGreen hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
-            Add
+            Update
           </button>
           {/* <Link */}
           {/* to="/user"> */}
